5f4b0a491278ae894c5ad42db910742c2eada877
whlong1 <whunterlong@gmail.com>
2022-07-25 09:08:54 -0400

    init

---
+++ models/profile.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+const profileSchema = new mongoose.Schema({
+  email: { type: String, required: true, lowercase: true, unique: true },
+  name: String,
+  photo: { type: String }
+},{
+  timestamps: true,
+})
+
+const Profile = mongoose.model('Profile', profileSchema)
+
+export { Profile }
