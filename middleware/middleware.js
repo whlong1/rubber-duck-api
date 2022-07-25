c9e8522e830a319e83e06c71887f1fdd341004a6
whlong1 <whunterlong@gmail.com>
2022-07-25 10:21:19 -0400

    add custom middleware file

---
+++ middleware/middleware.js
@@ -0,0 +1,9 @@
+function attributeAuthor(req, res, next) {
+  req.body.author = req.user.profile
+  next()
+}
+
+function removeEmptyFields(req, res, next) {
+  for (let key in req.body) req.body[key] === '' && delete req.body[key]
+  next()
+}
