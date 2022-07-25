4b6ad00bcbeeeb5c8ecf601f45533f68b6c717f9
whlong1 <whunterlong@gmail.com>
2022-07-25 12:36:19 -0400

    clean up profilesRouter

--- routes/profiles.js
+++ routes/profiles.js
@@ -10,7 +10,6 @@ const router = Router()
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
 router.get('/', checkAuth, profilesCtrl.index)
-router.get('/', checkAuth, profilesCtrl.index)
 router.get('/:id', checkAuth, profilesCtrl.show)
 router.post('/:id', checkAuth, profilesCtrl.follow)
 router.delete('/:id', checkAuth, profilesCtrl.unfollow)
