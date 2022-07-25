ebc6518bb005ed30e436cb1d9f0040be981c6b48
whlong1 <whunterlong@gmail.com>
2022-07-25 09:53:14 -0400

    build out profileSchema

--- models/profile.js
+++ models/profile.js
@@ -1,10 +1,40 @@
 import mongoose from 'mongoose'
 
 const profileSchema = new mongoose.Schema({
-  email: { type: String, required: true, lowercase: true, unique: true },
-  name: String,
-  photo: { type: String }
-},{
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
+  dob: {
+    type: String,
+    required: true,
+  },
+  occupation: {
+    type: String,
+    required: true,
+  },
+  zip: {
+    type: String,
+    required: true,
+  },
+  education: {
+    type: String,
+    required: true,
+  },
+  votes: [voteSchema],
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
+  interests: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category' }],
+}, {
   timestamps: true,
 })
 
