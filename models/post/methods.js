5b2ebf490aa9c7eebd68e3d74c262b6f3a20308c
whlong1 <whunterlong@gmail.com>
2022-08-01 18:03:21 -0400

    reoder findPostsAndIteration stages

--- models/post/methods.js
+++ models/post/methods.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 function findPostsAndIteration(topicId, search, sort, page) {
   const searchText = search ? search : ''
   const skipCount = page ? parseInt(page) * 10 : 0
-  const order = { recent: { 'iterations.createdAt': -1 }, popular: { 'iterations.rating': -1 } }
+  const order = { recent: { 'createdAt': -1 }, popular: { 'rating': -1 } }
   return this.aggregate([
     { $match: { topic: mongoose.Types.ObjectId(topicId) } },
     {
@@ -26,9 +26,6 @@ function findPostsAndIteration(topicId, search, sort, page) {
         ],
       }
     },
-    { $sort: sort ? order[sort] : order.recent },
-    { $skip: skipCount },
-    { $limit: 10 },
     {
       $project: {
         author: { $first: "$author" },
@@ -38,6 +35,9 @@ function findPostsAndIteration(topicId, search, sort, page) {
       }
     },
     { $match: { text: { $exists: true } } },
+    { $sort: sort ? order[sort] : order.recent },
+    { $skip: skipCount },
+    { $limit: 10 },
   ])
 }
 
